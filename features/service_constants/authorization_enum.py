dc73fba98fadff4852a6f2be268c4efb54a94782
Vishnu <vishnumj005@gmail.com>
2022-09-18 20:13:24 +0530

    update auth

--- features/service_constants/authorization_enum.py
+++ features/service_constants/authorization_enum.py
@@ -2,5 +2,5 @@ from enum import Enum
 
 
 class AuthorizationEnum(Enum):
-    AUTH_TOKEN = "5c61c28a-7e8c-443a-b218-2f93422bd18c"
-    AUTH_EMAIL = "vijayvichu.007@gmail.com"
+    AUTH_TOKEN = "#"
+    AUTH_EMAIL = "#"
